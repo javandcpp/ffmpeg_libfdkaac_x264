5ba80290f7d914e680379bb9a5a66931af929657
wanglibank <756280764@qq.com>
2017-08-03 18:57:09 +0800

    commit

---
+++ NativeApp/app/src/main/java/com/guagua/nativeapp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.guagua.nativeapp;
+
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.guagua.nativeapp.R;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView tvInfo = (TextView) findViewById(R.id.tvInfo);
+        File file=new File(Environment.getExternalStorageDirectory(),"123.wmv");
+        tvInfo.setText(nativeGetVideoInfo(file.getAbsolutePath()));
+    }
+
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+
+    public native String getStringUTF();
+
+    public native String nativeString();
+
+    public native String nativeGetVideoInfo(String path);
+}
