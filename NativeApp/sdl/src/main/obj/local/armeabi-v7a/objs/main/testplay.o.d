9dc45889957ea081289dd93347b3754405660cb5
wanglibank <756280764@qq.com>
2017-12-27 16:12:34 +0800

    fix

---
+++ NativeApp/sdl/src/main/obj/local/armeabi-v7a/objs/main/testplay.o.d
@@ -0,0 +1,212 @@
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/obj/local/armeabi-v7a/objs/main/testplay.o: \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/testplay.c \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libavcodec/avcodec.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libavutil/samplefmt.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libavutil/avutil.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libavutil/common.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libavutil/attributes.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libavutil/macros.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libavutil/version.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libavutil/avconfig.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libavutil/mem.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libavutil/error.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libavutil/rational.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libavutil/mathematics.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libavutil/intfloat.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libavutil/log.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libavutil/pixfmt.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libavutil/buffer.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libavutil/cpu.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libavutil/channel_layout.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libavutil/dict.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libavutil/frame.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libavcodec/version.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libavformat/avformat.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libavformat/avio.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libavformat/version.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libswscale/swscale.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libswscale/version.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_main.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_stdinc.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_config.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_platform.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/begin_code.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/close_code.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_config_android.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_assert.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_atomic.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_audio.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_error.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_endian.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_mutex.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_thread.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_rwops.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_clipboard.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_cpuinfo.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_events.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_video.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_pixels.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_rect.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_surface.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_blendmode.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_keyboard.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_keycode.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_scancode.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_mouse.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_joystick.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_gamecontroller.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_quit.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_gesture.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_touch.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_filesystem.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_haptic.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_hints.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_loadso.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_log.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_messagebox.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_power.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_render.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_system.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_timer.h \
+  /Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_version.h
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libavcodec/avcodec.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libavutil/samplefmt.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libavutil/avutil.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libavutil/common.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libavutil/attributes.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libavutil/macros.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libavutil/version.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libavutil/avconfig.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libavutil/mem.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libavutil/error.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libavutil/rational.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libavutil/mathematics.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libavutil/intfloat.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libavutil/log.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libavutil/pixfmt.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libavutil/buffer.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libavutil/cpu.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libavutil/channel_layout.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libavutil/dict.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libavutil/frame.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libavcodec/version.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libavformat/avformat.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libavformat/avio.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libavformat/version.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libswscale/swscale.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/libswscale/version.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_main.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_stdinc.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_config.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_platform.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/begin_code.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/close_code.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_config_android.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_assert.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_atomic.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_audio.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_error.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_endian.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_mutex.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_thread.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_rwops.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_clipboard.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_cpuinfo.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_events.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_video.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_pixels.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_rect.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_surface.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_blendmode.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_keyboard.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_keycode.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_scancode.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_mouse.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_joystick.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_gamecontroller.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_quit.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_gesture.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_touch.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_filesystem.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_haptic.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_hints.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_loadso.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_log.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_messagebox.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_power.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_render.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_system.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_timer.h:
+
+/Users/loneswordman/Downloads/ffmpeg_libfdkaac_x264/NativeApp/sdl/src/main/jni/src/../SDL/include/SDL_version.h:
